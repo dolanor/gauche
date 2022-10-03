27c38a94c1e861fe8fd354e47685fd948a6926a2
Tanguy ⧓ Herrmann <dolanorgit@evereska.org>
2022-10-03 16:01:12 +0200

    exp(buildkit): try env secrets in buildkit

--- buildkit/secrets/run.sh
+++ buildkit/secrets/run.sh
@@ -1,2 +1,10 @@
 export DOCKER_BUILDKIT=1
+
+# with file
 docker build --no-cache --progress=plain --secret id=mysecret,src=mysecret.txt .
+
+echo "==============="
+
+export MYSECRET="roflcopter"
+# with env
+docker build --no-cache --progress=plain --secret id=mysecret,env=MYSECRET .
